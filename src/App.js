bc764da63b3a6ff057cd46dd223c38f9778a5457
Somesh <somesh_nj2_076@masai.school>
2021-07-06 19:25:06 +0530

    react intro - react mobile os

--- src/App.js
+++ src/App.js
@@ -1,40 +1,12 @@
 import "./App.css";
-import { Joinus } from "./Components/React-Intro/Assignment/React Menu/Joinus";
-import { Settings } from "./Components/React-Intro/Assignment/React Menu/Settings";
-import { Login } from "./Components/React-Intro/Assignment/React Menu/Login";
-import { Contactus } from "./Components/React-Intro/Assignment/React Menu/Contactus";
-import { Search } from "./Components/React-Intro/Assignment/React Menu/Search";
-import { Help } from "./Components/React-Intro/Assignment/React Menu/Help";
-import { Home } from "./Components/React-Intro/Assignment/React Menu/Home";
-import { Download } from "./Components/React-Intro/Assignment/React Menu/Download";
-import { Menu } from "./Components/React-Intro/Assignment/React Menu/Menu";
+import { ReactMenu } from "./Components/React-Intro/Assignment/React Menu/ReactMenu";
+import { Mobile } from "./Components/React-Intro/Assignment/React Mobile OS/ReactMobileos";
 
 function App() {
-	const arr = [
-		"JOIN US",
-		"SETTINGS",
-		"LOGIN",
-		"CONTACT US",
-		"SEARCH",
-		"HELP",
-		"HOME",
-		"DOWNLOAD",
-	];
 	return (
 		<div className="App">
-			<h2>REACT MENU PART-I</h2>
-			<Joinus />
-			<Settings />
-			<Login />
-			<Contactus />
-			<Search />
-			<Help />
-			<Home />
-			<Download />
-			<h2>REACT MENU PART-II</h2>
-			{arr.map((el) => {
-				return <Menu menu={el} />;
-			})}
+			{/* <ReactMenu /> */}
+			<Mobile />
 		</div>
 	);
 }
