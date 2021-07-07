b2749412400f3895b46770bb94cb548ace15e831
Somesh <somesh_nj2_076@masai.school>
2021-07-07 19:47:43 +0530

    Completed React-Component-Designer-Card assignment

--- src/App.js
+++ src/App.js
@@ -1,26 +1,10 @@
 import React from "react";
 import "./App.css";
-import { ReactPaymentCard } from "./Components/React-States Styling/Assignment/ReactPaymentCard";
-
+import { ReactComponentDesignerCard } from "./Components/React-States Styling/Assignment/ReactComponentDesignerCard/ReactComponentDesignerCard";
 function App() {
 	return (
 		<div className="App">
-			<ReactPaymentCard
-				color={"#f5f5f5"}
-				heading={"Apple Gift"}
-				subheading={"Payment"}
-				devices={"MacOS - Mobile"}
-				date={"17 Sept 2020"}
-				logo={`https://www.logo.wine/a/logo/Apple_Inc./Apple_Inc.-Logo.wine.svg`}
-			/>
-			<ReactPaymentCard
-				color={"#FFA500"}
-				heading={"Amazon Gift"}
-				subheading={"Pay"}
-				devices={"Desktop - Mobile"}
-				date={"17/07/2020"}
-				logo={`https://cdn.worldvectorlogo.com/logos/amazon-icon-1.svg`}
-			/>
+			<ReactComponentDesignerCard />
 		</div>
 	);
 }
