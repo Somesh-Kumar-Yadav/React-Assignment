271787adfa1d328f46519c6e593dfed703096bd2
Somesh <somesh_nj2_076@masai.school>
2021-07-14 17:33:48 +0530

    React Hook Todos assignment completed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,40 @@
 import React from "react";
 import "./App.css";
-import { Timer } from "./Components/React-UseEffect/Timer/Timer";
+import { Todos } from "./Components/React-UseEffect/Todos/Todos";
 function App() {
+	const [formData, setFormData] = React.useState({});
+	const HandleChange = (e) => {
+		const { value, name, checked, type } = e.target;
+		setFormData({ ...formData, [name]: type === "checkbox" ? checked : value });
+	};
+	const GetData = (e) => {
+		e.preventDefault();
+		console.log(formData);
+	};
 	return (
 		<div className="App">
-			<Timer></Timer>
+			<Todos />
+			{/* <form onSubmit={GetData}>
+				<label htmlFor="name">
+					NAME:
+					<input
+						onChange={HandleChange}
+						type="text"
+						name="name"
+						placeholder="Enter your name"
+					></input>
+				</label>
+				<label htmlFor="isMarried">
+					isMarried:
+					<input
+						onChange={HandleChange}
+						type="checkbox"
+						name="isMarried"
+						placeholder="Enter your name"
+					></input>
+					<input onChange={HandleChange} type="submit"></input>
+				</label>
+			</form> */}
 		</div>
 	);
 }
--- src/Components/Counter.jsx
+++ src/Components/Counter.jsx
@@ -2,28 +2,28 @@ import "./Counter.css";
 import React from "react";
 const Counter =()=> {
 	const [counter, setCounter] = React.useState(0);
+	const useRef = React.useRef();
 	React.useEffect(() => {
-			console.log("mounting");
-			const clearValue=setInterval(()=> {
-				setCounter((preValue) => {
-					console.log("updating");
-					if (preValue === 10) {
-						clearInterval(clearValue)
-						return 0;
-					}
-					return preValue + 1;
-				});
-			}, 1000);
-			return () => {
-				console.log("cleaning")
-				clearInterval(clearValue);
-			}
+		HandleStart();
 		},[])
-	
+	const HandleStart = () => {
+		useRef.current = setInterval(() => {
+			setCounter(prev => prev + 1);
+		},1000)
+	}
+	const HandleStop = () => {
+		clearInterval(useRef.current)
+	}
+	const Reset = () => {
+		setCounter(0);
+		
+	}
 	return (
 		<div className="App">
 			<h3>Counter: {counter}</h3>
-			
+			<button onClick={HandleStart}>Start</button>
+			<button onClick={HandleStop}>Stop</button>
+			<button onClick={Reset}>Reset</button>
 		</div>
 	);
 }
